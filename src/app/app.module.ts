79366186a1a690a9988b187ea42e2312cd4bf362
Marcellinus Jerricho <marcellinus.jerricho@u.yale-nus.edu.sg>
2021-10-26 14:21:03 +0800

    route done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; // added automatically because we use cli module!
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+// bring in routher module, set up route.
+import { Router, RouterModule, Routes } from '@angular/router';
 
 // component
 import { AppComponent } from './app.component';
@@ -12,7 +14,15 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
+
+const appRoutes: Routes = [
+  // home -> just the tasks 
+  {path: '', component: TasksComponent},
+  {path: 'about', component: AboutComponent}
+]
 
 @NgModule({
   declarations: [ // for components
@@ -21,13 +31,17 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     ButtonComponent,
     TasksComponent,
     TaskItemComponent,
-    AddTaskComponent
+    AddTaskComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [ // for module
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true}) 
+    // enable tracing helps debug rout
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+// to find out what we're doing (couter)
 
 @Component({
   selector: 'app-header',
@@ -12,7 +14,8 @@ export class HeaderComponent implements OnInit {
   showAddTask: boolean = false;
   subscription!: Subscription;
 
-  constructor(private uiService: UiService) { 
+  constructor(private uiService: UiService, private router: Router) { // theses are providers
+    
     this.subscription = this.uiService
       .onToggle()
       .subscribe((value) => (this.showAddTask = value));
@@ -22,6 +25,10 @@ export class HeaderComponent implements OnInit {
 
   toggleAddTask() { //when btnclick is executed, this is called
      this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string) {
+    return (this.router.url == route); // boolean value
   }
 
 }
