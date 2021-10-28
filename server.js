b3c8b28eb6fe55ef7ce2c677337108d44a917ef7
Marcellinus Jerricho <marcellinus.jerricho@u.yale-nus.edu.sg>
2021-10-28 11:21:57 +0800

    build 1 fix try

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/task-tracker'));
+app.use(express.static('./dist/ngprojec1'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/task-tracker/'}),
+    res.sendFile('index.html', {root: 'dist/ngproject1/'}),
 );
 
 // Start the app by listening on the default Heroku port
